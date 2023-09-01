bfa57f0c6029811268dac87a77994f1cb4bf50f5
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-09-01 16:21:05 +0700

    membuat Contact Model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,19 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class User extends Model
+class Contact extends Model
 {
-    protected $table = "users";
+    protected $table = "contacts";
     protected $primaryKey = "id";
     protected $keyType = "int";
     public $incrementing = true;
     public $timestamps = true;
 
-    public function contact():HasMany
+    public function user():BelongsTo
     {
-        return $this->hasMany(Contact::class, "user_id", "id");
+        return $this->belongsTo(User::class, "user_id", "id");
     }
+
 
 }
