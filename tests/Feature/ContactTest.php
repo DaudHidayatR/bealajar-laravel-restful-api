1cf7871311e75d8fdcc6ce65776bd85fb6bdc2ae
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-09-03 20:41:48 +0700

    membuat Remove Contact API
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -177,4 +177,36 @@ class ContactTest extends TestCase
             ]);
     }
 
+    public function testDeleteSuccess()
+    {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+        $contact = Contact::first();
+
+        $this->delete('/api/contacts/'.$contact->id,[],
+            [
+                'Authorization' => 'test'
+            ])->assertStatus(200)
+            ->assertJson([
+                'data' => 'true'
+            ]);
+    }
+    public function testDeleteNotFound()
+    {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+        $contact = Contact::first();
+
+        $this->delete('/api/contacts/'.($contact->id+1),[],
+            [
+                'Authorization' => 'test'
+            ])->assertStatus(404)
+            ->assertJson([
+                'errors' => [
+                    'message' => [
+                        'Contact not found'
+                    ]
+                ]
+            ]);
+    }
+
+
 }
